a645ef9a1ae65209316239a454fae89f07b5f6e1
John Doe <gmiello411@gmail.com>
2025-03-10 20:13:10 -0300

    ajustes 1

---
+++ Projeto1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Lista1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Digite um número:");
+            if (int.TryParse(Console.ReadLine(), out int numero))
+            {
+                if (numero <= 10)
+                {
+                    Console.WriteLine("Número menor ou igual a 10!");
+                }
+                else
+                {
+                    Console.WriteLine("Número maior que 10!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro.");
+            }
+        }
+    }
+}
+    
