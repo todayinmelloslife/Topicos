977159dfddfa88f341df1f5f77f78db38b9e32a9
John Doe <gmiello411@gmail.com>
2025-03-10 20:01:17 -0300

    first commit

---
+++ Aula2/firstProject.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Hello, World!";
+            
+            for (int i = 0; i < frase.Length; i++) {
+                Console.WriteLine(frase[i]);
+            }
+        }
+    }
+}
