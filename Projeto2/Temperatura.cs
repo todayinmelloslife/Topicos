823122c8b94857a9e132b5b57ee55ece77d95654
John Doe <gmiello411@gmail.com>
2025-03-10 20:36:30 -0300

    ainda nada

--- Projeto2/Temperatura.cs
+++ Projeto2/Temperatura.cs
@@ -1,5 +1,26 @@
 ﻿using System;
-
+namespace Projeto2
+{
+    class Temperatura
+    {
+        static void Main()
+        {
+            Console.Write("Digite a temperatura em graus Celsius: ");
+            string input = Console.ReadLine();
+            
+            if (double.TryParse(input, out double celsius)) //TRYPERSE é aquela funcao que converte o valor da string em um valor double, né?
+            {
+                double fahrenheit = (9.0 / 5.0) * celsius + 32;
+                
+                Console.WriteLine($"A temperatura em graus Fahrenheit é: {fahrenheit:F2}"); // O F2 é para limitar o número de casas decimais 
+            }
+            else
+            {
+                Console.WriteLine("Por favor, insira um valor numérico válido.");
+            }
+        }
+    }
+}
 class Program
 {
     static void Main()
@@ -19,3 +40,4 @@ class Program
         }
     }
 }
+
